499bbf184fabebae70edd67f32422b1970f75d30
irmasaquiza <nachitomv2005@hotmail.com>
2025-12-25 14:27:57 -0500

    mejora del LOGIN AHORA CUANDO APLASTEN EN INICIAR SESION PONEN SUS CREDENCIALES Y LISTO

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,10 @@ use App\Models\User;
 use App\Http\Controllers\ProductoController;
 use App\Http\Controllers\ClientesController;
 use GuzzleHttp\Client;
+use App\Http\Controllers\LoginController;
 
+
+// ECOMMERCE
 Route::get('/', function () {
 
     return view('Ecommerce.dashboard');
@@ -18,23 +21,26 @@ Route::get('/portada', function () {
     return view('Ecommerce.dashboard');
 })->name('portada.index');
 
-
-Route::get('/demo-login/{id}', function ($id) {
-    $user = User::findOrFail($id);
-    session(['user_id' => $user->id]);
-
-    return "Sesión iniciada como {$user->name} ({$user->rol})";
-});
-
-
 Route::get('/catalogo',[CatalogoController::class,'index'])->name('catalogo.index');
 Route::get('/detalle/{id}',[CatalogoController::class,'show'])->name('catalogo.detalle');
 
 
 
 
+// LOGIN 
+Route::controller(LoginController::class)->group(function () {
+    Route::get('/login', function () {
+        return view('auth.login');
+    })->name('login.form');
+
+    Route::post('/login', 'login')->name('login.post');
+});
+
+
+
+// REDIRECCION A LA GESTION DE PRODUCTOS, PROVEEDORES Y CLIENTES
 Route::middleware(['rol:gerente_bodega'])
-     ->prefix('admin')
+     ->prefix('admin/bodega')
      ->group(function () {
 
          Route::get('/', function () {
@@ -43,23 +49,26 @@ Route::middleware(['rol:gerente_bodega'])
 
          Route::resource('productos',ProductoController::class);
      });
-// Route::middleware(['rol:gerente_compras'])
-//     ->prefix('admin/compras')
-//     ->group(function () {
 
-//         Route::get('/', function () {
-//             return 'Backoffice Compras 🧾';
-//         });
+Route::middleware(['rol:gerente_compras'])
+     ->prefix('admin/compras')
+     ->group(function () {
 
-//     });
+         Route::get('/', function () {
+            return 'Backoffice Compras 🧾';
+         });
 
-// Route::middleware(['rol:gerente_ventas'])
-//     ->prefix('admin/ventas')
-//     ->group(function () {
+     });
 
-//         Route::get('/', function () {
-//             return 'Backoffice Ventas 💰';
-//         });
-//     });
+ Route::middleware(['rol:gerente_ventas'])
+     ->prefix('admin/ventas')
+     ->group(function () {
+         Route::get('/', function () {
+             return 'Backoffice Ventas 💰';
+         });
+
+         Route::resource('clientes',ClientesController::class);
+
+     });
 
 
