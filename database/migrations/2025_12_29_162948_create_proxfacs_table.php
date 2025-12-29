d352713909c892c5b9eac1f27f628aa2d14e8e11
anaberru <anaberru21@gmail.com>
2025-12-29 11:46:19 -0500

    .

--- database/migrations/2025_12_29_162948_create_proxfacs_table.php
+++ database/migrations/2025_12_29_162948_create_proxfacs_table.php
@@ -11,8 +11,21 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('proxfacs', function (Blueprint $table) {
-            $table->id();
+        Schema::create('proxfac', function (Blueprint $table) {
+            $table->char('id_factura', 15);
+            $table->char('id_producto',15);
+            $table->integer('pxf_cantidad');
+            $table->decimal('pxf_precio',10,2);
+            $table->decimal('pxf_subtotal',10,2);
+            $table->enum('pxf_estado',['ABI','ANU','APR'])->default('ABI');
+            $table->primary(['id_factura', 'id_producto']);
+
+            $table->foreign('id_factura')
+                ->references('id_factura')
+                ->on('facturas');
+            $table->foreign('id_producto')
+                ->references('id_producto')
+                ->on('productos');
             $table->timestamps();
         });
     }
@@ -22,6 +35,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('proxfacs');
+        Schema::dropIfExists('proxfac');
     }
 };
